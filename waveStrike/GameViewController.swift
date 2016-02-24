909c4c9f2e9bb51fa414916c04d4e881ab6a5684
igmstudent <igmstudent@c2025m-19.main.ad.rit.edu>
2016-02-24 18:15:14 -0500

    GameScene now has blue background; Added ship assets and MyUtils.

--- waveStrike/GameViewController.swift
+++ waveStrike/GameViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by igmstudent on 2/24/16.
 //  Copyright (c) 2016 igmstudent. All rights reserved.
-//  testing
+//
 
 import UIKit
 import SpriteKit
@@ -13,41 +13,15 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+        let scene = GameScene(size:CGSize(width: 2048, height: 1536))
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .AspectFill
+        skView.presentScene(scene)
     }
-
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override func prefersStatusBarHidden() -> Bool {
+    override func prefersStatusBarHidden() -> Bool  {
         return true
     }
 }
