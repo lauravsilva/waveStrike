f883fc90fbc099b889804359362ca63448cdeeae
igmstudent <igmstudent@c2025m-17.main.ad.rit.edu>
2016-02-28 18:42:37 -0500

    (Laura) Added touches end and temp projectile

--- waveStrike/GameScene.swift
+++ waveStrike/GameScene.swift
@@ -34,7 +34,7 @@ class GameScene: SKScene
         let myLabel = SKLabelNode(fontNamed:"Sailor-Beware")
         myLabel.text = "Wave Strike"
         myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMaxY(self.frame)-100)
         self.addChild(myLabel)
         
         //Player
@@ -99,6 +99,50 @@ class GameScene: SKScene
         let touchLocation = touch.locationInNode(self)
         sceneTouched(touchLocation)
     }
+    
+    
+    // Touch end event (fire bullets)
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        // Choose one of the touches to work with
+        guard let touch = touches.first else {
+            return
+        }
+        let touchLocation = touch.locationInNode(self)
+        
+        
+        // Set up initial location of projectile
+        let projectile = SKSpriteNode(imageNamed: "projectile")
+        projectile.position = player.position
+        
+        projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2)
+        projectile.physicsBody?.dynamic = true
+        projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
+        projectile.physicsBody?.contactTestBitMask = PhysicsCategory.Monster
+        projectile.physicsBody?.collisionBitMask = PhysicsCategory.None
+        projectile.physicsBody?.usesPreciseCollisionDetection = true
+        
+        // Determine offset of location to projectile
+        let offset = touchLocation - projectile.position
+        
+        addChild(projectile)
+        
+        // Get the direction of where to shoot
+        let direction = offset.normalized()
+        
+        // Make it shoot far enough to be guaranteed off screen
+        let shootAmount = direction * 2000
+        
+        // Add the shoot amount to the current position
+        let realDest = shootAmount + projectile.position
+        
+        // Create the actions
+        let actionMove = SKAction.moveTo(realDest, duration: 2.0)
+        let actionMoveDone = SKAction.removeFromParent()
+        projectile.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+        
+    }
+    
 }
 
 
--- waveStrike/MyUtils.swift
+++ waveStrike/MyUtils.swift
@@ -37,6 +37,14 @@ extension UIColor {
     }
 }
 
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+    static let Monster   : UInt32 = 0b1       // 1
+    static let Projectile: UInt32 = 0b10      // 2
+}
+
+
 func + (left: CGPoint, right: CGPoint) -> CGPoint {
   return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
